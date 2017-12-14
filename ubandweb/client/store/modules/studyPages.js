d38fc7a902e9c6a02bcbb317f7599000a707e8b2
877927262 <877927262@qq.com>
2017-12-14 22:20:01 +0800

    不知为何出现了延迟

--- ubandweb/client/store/modules/studyPages.js
+++ ubandweb/client/store/modules/studyPages.js
@@ -67,35 +67,13 @@ const state={
     }]
   }],
   // 当前大夫的工作信息
-  theDoctor:[{
-    date:'星期一',
-    am:'11人',
-    pm:'111人'
-  },{
-    date:'星期二',
-    am:'22人',
-    pm:'222人'
-  },{
-    date:'星期三',
-    am:'3人',
-    pm:'333人'
-  },{
-    date:'星期四',
-    am:'4人',
-    pm:'444人'
-  },{
-    date:'星期五',
-    am:'5人',
-    pm:'555人'
-  },{
-    date:'星期六',
-    am:'6人',
-    pm:'666人'
-  },{
-    date:'星期日',
-    am:'7人',
-    pm:'777人'
-  }],
+  theDoctor:[
+    // {
+    // date:'星期一',
+    // am:'11人',
+    // pm:'111人'
+    // }
+  ],
   //医院科室列表
   hospitalDepartmentList:[
     // {
@@ -111,23 +89,9 @@ const state={
     // name:'感冒',
     // banner:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512111646555&di=34daf8b3e3d8889f4c3b594d4819f4c8&imgtype=0&src=http%3A%2F%2Fwww.th7.cn%2Fd%2Ffile%2Fp%2F2016%2F08%2F01%2Fb2abda9a1e99731a8d9bf34399af1003.jpg'
     // }
-  ],
-  //当前大夫
-  DepartmentDoctorList:[
-  //   {
-  //   id:'007',
-  //   name:'张三疯'
-  // }
-  ],
-  //当前病症大夫
-  illnessDoctorList:[
-    {
-    id:'01',
-    name:'大柱子'
-  },{
-    id:'02',
-    name:'三柱子'
-  }]
+  ]
+
+
 };
 const getters={
 
@@ -148,6 +112,11 @@ const mutations={
 
   setCurrentIllnessDoctorList(state,{illnessDoctorList}){
     state.illnessDoctorList=illnessDoctorList;
+
+  },
+
+  setCurrentDoctorWorkList(state,{theDoctor}){
+    state.theDoctor=theDoctor;
 
   }
 };
@@ -199,8 +168,20 @@ const actions={
     }).catch(function (err) {
       throw err;
     })
-  }
+  },
+  //theDoctor当前大夫的工作信息
+  getCurrentDoctorWorkList({commit},params){
+    fetch('http://127.0.0.1:3000/appointment/getCurrentDoctorWorkList?&doctorId='+params.doctorId).then(function(response) {
+      response.json().then(function(res){
+        commit('setCurrentDoctorWorkList',{theDoctor:res})
 
+      }).catch(function (err) {
+        throw err;
+      })
+    }).catch(function (err) {
+      throw err;
+    })
+  }
 
 }
 
