8ab93b398f2ca5309a6d84d4ba3296f1086f93ef
877927262 <877927262@qq.com>
2017-12-14 23:39:22 +0800

    一行代码引发的熬夜，我醉了

--- ubandweb/client/store/modules/studyPages.js
+++ ubandweb/client/store/modules/studyPages.js
@@ -89,7 +89,16 @@ const state={
     // name:'感冒',
     // banner:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512111646555&di=34daf8b3e3d8889f4c3b594d4819f4c8&imgtype=0&src=http%3A%2F%2Fwww.th7.cn%2Fd%2Ffile%2Fp%2F2016%2F08%2F01%2Fb2abda9a1e99731a8d9bf34399af1003.jpg'
     // }
+  ],
+  //当前大夫
+  DepartmentDoctorList:[
+    //   {
+    //   id:'007',
+    //   name:'张三疯'
+    // }
   ]
+
+
 
 
 };
--- ubandweb/client/views/studyPages/myCourse.vue
+++ ubandweb/client/views/studyPages/myCourse.vue
@@ -6,7 +6,7 @@
     <div class="left">
       <h5>请选择大夫</h5>
       <b-nav vertical class="left-nav" >
-        <b-nav-item v-for="(item,index) in DepartmentDoctorList" :key="index" @click="change(index,item.doctor)" :class={active:isActive(index)}>{{item.name}}</b-nav-item>
+        <b-nav-item v-for="(item,index) in DepartmentDoctorList" :key="index" @click="change(index)" :class={active:isActive(index)}>{{item.name}}</b-nav-item>
       </b-nav>
     </div>
     <div class="right">
@@ -26,36 +26,14 @@ export default {
     //按科室查找大夫
     DepartmentDoctorList(){
       return this.$store.state.studyPages.DepartmentDoctorList;
-    },
-
-    departmentId(){
-      return this.$route.params.departmentId;
-    },
-    illnessId(){
-      return this.$route.params.illnessId;
     }
   },
   methods:{
     isActive(index) {
       return this.currentIndex === index
     },
-    change(index,doctorId){
-      this.currentIndex = index;
-      this.$store.dispatch('getCurrentDoctorWorkList',{doctorId})
-
-    }
-  },
-  created(){
-    //动态获取当前的大夫列表
-    if(this.departmentId){
-      //如果是按照科室查询的
-      this.$store.dispatch('getCurrentDepartmentDoctorList',{departmentId:this.departmentId})
-    }else if(this.illnessId){
-      //如果是按照病症查询的
-      this.$store.dispatch('getCurrentIllnessDoctorList',{illnessId:this.illnessId})
-
-    }else{
-      console.log('出错了')
+    change(index){
+      this.currentIndex = index
     }
   }
 }
