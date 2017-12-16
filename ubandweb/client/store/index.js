999891e3c4b694a2af79da6be685b2aca4332a49
877927262 <877927262@qq.com>
2017-12-16 15:53:59 +0800

    又删除了一些东西

--- ubandweb/client/store/index.js
+++ ubandweb/client/store/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import account from './modules/account'
-import config from './modules/config'
 import studyPages from './modules/studyPages'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    account,
-    config,
     studyPages
   }
 })
