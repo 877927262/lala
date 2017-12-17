3d53df44e6222112508e8db8a46fd277b617342d
877927262 <877927262@qq.com>
2017-12-17 14:39:34 +0800

    header写好了，不改了

--- ubandweb/client/components/u-header.vue
+++ ubandweb/client/components/u-header.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="u-header">
+    <h2 class="title pull-left">欢迎使用社区挂号平台</h2>
     <b-button to="/studyPage" class="login-btn u-btn u-btn-primary">
-      登录
+      我要挂号
     </b-button>
   </div>
 </template>
